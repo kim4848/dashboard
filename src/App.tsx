76c19b42ab19e4a93a91926a7744b98e16a3022b
Kim <kj@kjsoft.dk>
2022-09-17 07:25:31 +0200

    Fixed Menu Open/Close issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { useState } from 'react';
-import './App.css';
-import { Button, IconButton, Toolbar } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import Menu from './Menu';
+import { useState } from "react";
+import "./App.css";
+import { Button, Toolbar } from "@mui/material";
+import Menu from "./Menu";
 
 function App() {
-  const [count, setCount]= useState(0)
-  const [showMenu,setShowMenu]=useState(false)
+  const [count, setCount] = useState(0);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const hide = () => {
+    console.log("click");
+    setShowMenu(false);
+  };
   return (
-    <div className="App">     
+    <div className="App">
       <Toolbar>
-      <IconButton onClick={()=>setShowMenu(false)}>
-              <ChevronLeftIcon />
-            </IconButton>
-        <IconButton onClick={()=>setShowMenu(true)}>
-          
-          <MenuIcon></MenuIcon>
-        </IconButton>       
-        {Menu(showMenu)}
-        </Toolbar> 
-      <Button onClick={()=>{setCount(count+1)}} variant="outlined">Test</Button>
-      <div>
-      Count: {count}
-      </div>
+        <Menu open={showMenu}></Menu>
+      </Toolbar>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+        variant="outlined"
+      >
+        Test
+      </Button>
+      <div>Count: {count}</div>
     </div>
   );
 }
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,58 +1,39 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { IconButton } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
-
-export default function Menu(open:boolean) {
-const [showMenu, setShowMenu]=React.useState(false)
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event &&
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      
-    };
+type Anchor = "top" | "left" | "bottom" | "right";
+interface MenuState {
+  open: boolean;
+}
+export default function Menu(open: MenuState) {
+  const [showMenu, setShowMenu] = React.useState(open.open);
 
   const list = (anchor: Anchor) => (
-    <Box
-      sx={{ width:  250 }}
-      role="presentation"     
-    >
+    <Box sx={{ width: 250 }} role="presentation">
       <List>
-       
-          <ListItem  disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                { <InboxIcon />}
-              </ListItemIcon>
-              <ListItemText primary={"test"} />
-            </ListItemButton>
-          </ListItem>
-       
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>{<InboxIcon />}</ListItemIcon>
+            <ListItemText primary={"test"} />
+          </ListItemButton>
+        </ListItem>
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        {["All mail", "Trash", "Spam"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
@@ -67,22 +48,25 @@ const [showMenu, setShowMenu]=React.useState(false)
   );
 
   return (
-    <div>     
-        <React.Fragment>
-          <Button onClick={()=>setShowMenu(true)}>Open</Button>
-          <Drawer
-            anchor={"left"}
-            open={open}
-          >  <Divider></Divider>
-          
-               <IconButton onClick={()=>setShowMenu(false)}>
-              <ChevronLeftIcon />
-            </IconButton>
-        <IconButton onClick={()=>setShowMenu(true)}></IconButton>
+    <div>
+      <React.Fragment>
+        <IconButton onClick={() => setShowMenu(true)}>
+          <MenuIcon></MenuIcon>
+        </IconButton>
+        <Drawer anchor={"left"} open={showMenu}>
+          {" "}
           <Divider></Divider>
-          {list("left")}           
-          </Drawer>
-        </React.Fragment>    
+          <IconButton
+            sx={{ width: 50, height: 50 }}
+            size="small"
+            onClick={() => setShowMenu(false)}
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+          <Divider></Divider>
+          {list("left")}
+        </Drawer>
+      </React.Fragment>
     </div>
   );
 }
